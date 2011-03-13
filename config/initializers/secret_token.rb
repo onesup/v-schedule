8f738e2a557867de00753594302a9c9c257745d9
onesup <onesup.lee@gmail.com>
2011-03-14 00:48:13 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VSchedule::Application.config.secret_token = '71ed806e83c39455a77318d31b2f2f82924d9a671f55acdf1905a0738cfbd6f999cfcb2bee93fd26ff2a8ed64d097d08fe7ae6fcf81cf7e1ddac6f8de3f7340c'

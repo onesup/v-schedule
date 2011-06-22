7974fe81b3bd85d418e1d45a40f251ea5f2ca1b5
onesup <onesup.lee@gmail.com>
2011-06-22 15:16:13 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -59,7 +59,7 @@ VSchedule::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-  root :to => "vaccinations#index"
+  root :to => "main/babies#new"
 
   # See how all your routes lay out with "rake routes"
 
